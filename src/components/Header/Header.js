d21a85b7de6e70be86bac5240ef785edeaef20b4
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-06-18 09:46:04 +0300

    Added logo apearing animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import styles from './Header.module.css';
 import logo from '../../img/phonebook.png';
+import popTransition from '../transitions/pop.module.css';
 
-const Header = () => (
-  <>
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <nav className={styles.navigation}>
-          <a href="\" className={styles.logo}>
-            <img src={logo} alt="Phonebook logo" />
-          </a>
-          <ul className={styles.list}>
-            <li>Home</li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  </>
-);
+export default class Header extends Component {
+  state = { animateLogo: false };
 
-export default Header;
+  componentDidMount() {
+    this.setState({
+      animateLogo: true,
+    });
+  }
+
+  render() {
+    const { animateLogo } = this.state;
+    return (
+      <>
+        <header className={styles.header}>
+          <div className={styles.container}>
+            <nav className={styles.navigation}>
+              <CSSTransition
+                in={animateLogo}
+                timeout={500}
+                unmountOnExit
+                classNames={popTransition}
+              >
+                <a href="\" className={styles.logo}>
+                  <img src={logo} alt="Phonebook logo" />
+                </a>
+              </CSSTransition>
+              <ul className={styles.list}>
+                <li>Home</li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+      </>
+    );
+  }
+}
